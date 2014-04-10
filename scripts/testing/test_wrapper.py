d802b73d479ac35f3ef27203d0f3b4a8036b8e83
alfongj <alfongj@gmail.com>
2014-04-09 17:42:06 -0700

    Fixed bug in previous 2 commits

--- scripts/testing/test_wrapper.py
+++ scripts/testing/test_wrapper.py
@@ -2,17 +2,18 @@
 # It needs to be modified to run actual Akaros tests and parse stdout or
 # something to extract test results and pass/fail based on them.
 try:
-    import unittest2 as unittest
+	import unittest2 as unittest
 except ImportError:
-    import unittest
+	import unittest
 
 class TestWrapper(unittest.TestCase):
-    def test_kern(self):
-        self.assertEqual(10, 7 + 3)
-        self.assertEqual(10, 7 + 3)
-        # 'tmp/akaros_out.txt'
+	@attr('kernel','cross-compiler','userspace','busybox')
+	def test_kern(self):
+		self.assertEqual(10, 7 + 3)
+		self.assertEqual(10, 7 + 3)
+		# 'tmp/akaros_out.txt'
 
-    # def test_fail(self):
-    #     self.assertEqual(11, 7 + 3)
+	# def test_fail(self):
+	#	 self.assertEqual(11, 7 + 3)
 
 # TODO: for each test, tag: @attr('kernel','cross-compiler','userspace','busybox')
