5623467788ebc0d7f01d2d0a4a7c7a557f5a80d8
alfongj <alfongj@gmail.com>
2014-04-10 11:11:18 -0700

    Continued testing jenkins output

--- scripts/testing/launcher.sh
+++ scripts/testing/launcher.sh
@@ -239,14 +239,21 @@ do
 	TESTS_TO_RUN="$TESTS_TO_RUN -a $COMPONENT"
 done
 
-echo -e "<testsuite name=\"nosetests\" tests=\"2\" errors=\"1\" failures=\"0\" skip=\"0\">" > $TEST_OUTPUT_DIR/test_output.xml
+echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $TEST_OUTPUT_DIR/test_output.xml
+echo -e "<testsuite name=\"nosetests\" tests=\"2\" errors=\"1\" failures=\"0\" skip=\"0\">" >> $TEST_OUTPUT_DIR/test_output.xml
 echo -e "<testcase classname=\"uno.dos.Tres\" name=\"cuatro\" time=\"0.000\">" >> $TEST_OUTPUT_DIR/test_output.xml
 echo -e "<error type=\"exceptions.IOError\" message=\"I like turtles\">"  >> $TEST_OUTPUT_DIR/test_output.xml
 echo -e "</error>" >> $TEST_OUTPUT_DIR/test_output.xml
 echo -e "</testcase>" >> $TEST_OUTPUT_DIR/test_output.xml
 echo -e "<testcase classname=\"uno.tres.Cuatro\" name=\"Cinco\" time=\"0.000\"/>" >> $TEST_OUTPUT_DIR/test_output.xml
 echo -e "</testsuite>" >> $TEST_OUTPUT_DIR/test_output.xml
-
+echo -e "<testsuite name=\"eyetests\" tests=\"2\" errors=\"1\" failures=\"0\" skip=\"0\">" >> $TEST_OUTPUT_DIR/test_output.xml
+echo -e "<testcase classname=\"uno.dos.Tres\" name=\"cuatro\" time=\"0.000\">" >> $TEST_OUTPUT_DIR/test_output.xml
+echo -e "<error type=\"exceptions.IOError\" message=\"I like turtles\">"  >> $TEST_OUTPUT_DIR/test_output.xml
+echo -e "</error>" >> $TEST_OUTPUT_DIR/test_output.xml
+echo -e "</testcase>" >> $TEST_OUTPUT_DIR/test_output.xml
+echo -e "<testcase classname=\"dos.tres.Cuatro\" name=\"Cinco\" time=\"0.000\"/>" >> $TEST_OUTPUT_DIR/test_output.xml
+echo -e "</testsuite>" >> $TEST_OUTPUT_DIR/test_output.xml
 
 # nosetests $TEST_DIR/test_wrapper.py --with-xunit \
 # 	--xunit-file=$TEST_OUTPUT_DIR/test_output.xml $TESTS_TO_RUN
